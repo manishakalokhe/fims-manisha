04e55cc2fa0ae4271fdb518fbd7d44322e2e331d
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-26 11:20:48 +0530

    Updated FIMSOfficeInspection.tsx

--- src/components/FIMSOfficeInspection.tsx
+++ src/components/FIMSOfficeInspection.tsx
@@ -347,7 +347,7 @@ export const FIMSOfficeInspection: React.FC<FIMSOfficeInspectionProps> = ({
     return `OFF-${year}${month}${day}-${time}`;
   };
 
-  const handleSubmit = async (isDraft: boolean = false) => {
+  const handleSubmit = async (isDraft: boolean = false) => {debugger
     try {
       setIsLoading(true);
 
