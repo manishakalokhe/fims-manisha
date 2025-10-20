64e7e99273ef572be3fa3aab4f36c75a8e926d9f
manishakalokhe <info@dpulseai.onmicrosoft.com>
2025-10-20 15:44:07 +0530

    Updated FIMSNewInspection.tsx

--- src/components/FIMSNewInspection.tsx
+++ src/components/FIMSNewInspection.tsx
@@ -237,7 +237,7 @@ export const FIMSNewInspection: React.FC<FIMSNewInspectionProps> = ({
             {t('fims.selectCategory')}
           </p>
         </div>
-
+        
         {/* Inspection Type Selection - Updated to support more forms */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
           {/* Anganwadi Inspection */}
