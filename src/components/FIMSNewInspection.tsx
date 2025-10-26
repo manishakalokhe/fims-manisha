fdd3c34548569f34ed6a75ee62560bb082883763
manishakalokhe <info@dpulseai.onmicrosoft.com>
2025-10-26 11:21:45 +0530

    Updated FIMSNewInspection.tsx

--- src/components/FIMSNewInspection.tsx
+++ src/components/FIMSNewInspection.tsx
@@ -206,7 +206,7 @@ export const FIMSNewInspection: React.FC<FIMSNewInspectionProps> = ({
       );
     }
 
-    if (selectedInspectionType === '') {jilhastariya_inspection_form
+    if (selectedInspectionType === 'jilhastariya_inspection_form') {
   return (
     <JilhastrariyaInspectionForm
       user={user}
