d151ed5347261dd49792dd20814e1d0158be9349
manishakalokhe <info@dpulseai.onmicrosoft.com>
2025-10-26 13:46:54 +0530

    Updated FIMSNewInspection.tsx

--- src/components/FIMSNewInspection.tsx
+++ src/components/FIMSNewInspection.tsx
@@ -445,7 +445,7 @@ export const FIMSNewInspection: React.FC<FIMSNewInspectionProps> = ({
                     <p>• सर्वसाधारण तपासणीचे ठिकाण</p>
                     </>
                 ) : (
-                  <></>
+                 
                 )}
               </div>
               
